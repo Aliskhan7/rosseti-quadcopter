c73be4456701cd2ef020fb3edd05c89471454fac
midnightelf <abdulovk20@gmail.com>
2021-12-04 03:45:32 +0000

    refactor: HotFix

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -22,21 +22,3 @@ Route::get('/', function () {
 Route::get('csrf', function() {
     return csrf_token();
 });
-
-
-// Fix
-
-Route::get('/init', function () {
-    Artisan::call('key:generate');
-});
-
-Route::get('/quadcopter-fix', function () {
-    DB::table('quadcopters')->update(['img'=>'https://expovision.tech/assets/tmp/quadcopter-dark.png']);
-
-    return 'картинабчки заменены';
-});
-
-Route::get('/fresh-db', function () {
-    Artisan::call('migrate:fresh');
-    Artisan::call('db:seed');
-});
